51b7c6c456407aa66a4df7c839fe5891327365c0
jihaifeng <1104128141@qq.com>
2017-07-13 14:52:08 +0800

    Initial commit

---
+++ app/src/main/java/com/jihf/joke/MyApplication.java
@@ -0,0 +1,32 @@
+package com.jihf.joke;
+
+import com.jihf.lib.app.BaseApplication;
+import com.jihf.lib.constans.UrlBean;
+import com.jihf.lib.share.ShareBean;
+
+/**
+ * Func：
+ * Desc:
+ * Author：JHF
+ * Data：2017-07-13 11:12
+ * Mail：jihaifeng@raiyi.com
+ */
+
+public class MyApplication extends BaseApplication {
+
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+
+  @Override public UrlBean initUrl() {
+    return null;
+  }
+
+  @Override public ShareBean initShare() {
+    return null;
+  }
+
+  @Override public String initUmengAppKey() {
+    return null;
+  }
+}
